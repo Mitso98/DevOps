6805288dfc8ab21d68c92c47c9d73b7feeeff609
Mostafa Ahmed <mostafa.ahmed19293949@gmail.com>
2022-07-28 04:11:52 +0200

    edit pipeline

--- hello.py
+++ hello.py
@@ -1,10 +1,9 @@
-
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def index():
+def hello():
     return "<p>Hello World!</p>"
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
