c48da3f743e301aee2c310d2c5db274a6de4cfdd
EC2 Default User <ec2-user@ip-172-31-7-114.ec2.internal>
2022-07-29 08:21:42 +0000

    add smoke test

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     # smoke test will not find "hello" so it will trigger roll back
-    return "<p>Udacity! V3</p>" 
+    return "<p>Udacity! V4</p>" 
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80,debug=True) 
